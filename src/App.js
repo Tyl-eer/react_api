6300e68fc5f014009c61b2d8cd7e7a8d1497492c
ty-russ <tiruskimani3@gmail.com>
2020-11-28 13:13:11 +0300

    consume api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+ import React, {Component} from 'react';
+ import Contacts from './components/contacts';
+ //import Posts from './components/posts';
+
+ class App extends Component {
+   //render () {
+    // return (
+      
+      state = {
+        contacts: []
+      }
+      componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ contacts: data })
+        })
+        .catch(console.log)
+      }
+      render() {
+        return (
+          <Contacts contacts={this.state.contacts} />
+        )
+      }
+
+      // state = {
+      //   posts: []
+      // }
+      // componentDidMount() {
+      //   fetch('https://jsonplaceholder.typicode.com/posts')
+      //   .then(res => res.json())
+      //   .then((data) => {
+      //     this.setState({ posts: data })
+      //   })
+      //   .catch(console.log)
+      // }
+
+      // render() {
+      //   return (
+      //     <Posts posts={this.state.posts} />
+      //   )
+      // }
+    
+ }
 
 export default App;
