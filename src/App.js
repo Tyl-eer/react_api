069ea457bd45c21df0c62b3864e4ce698f3f4dd1
Tyl-eer <blssdtyler03@gmail.com>
2020-12-08 11:34:34 +0300

    my first json-placeholder commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 
 
- import React, {Component} from 'react';
+ import React, {useState, useEffect} from 'react';
  import Contacts from './components/contacts';
  //import Posts from './components/posts';
 
- class App extends Component {
+function App () {
    //render () {
-    // return (
-      
-      state = {
-        contacts: []
-      }
-      componentDidMount() {
+
+    const [setdata, setcontacts] = useState('')
+      // return (
+      useEffect(() => {
         fetch('http://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
-        .then((data) => {
-          this.setState({ contacts: data })
-        })
-        .catch(console.log)
-      }
-      render() {
+        .then((data) => setdata(data))
+          .then((contacts) => setcontacts(contacts))
+        .catch(console.log (err))
+      }, [])
+        
+      // )
+      // }
+      // render() {
         return (
           <Contacts contacts={this.state.contacts} />
         )
-      }
+      // }
 
       // state = {
       //   posts: []
