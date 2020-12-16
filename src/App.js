92a9c627afbf4226645ae58a3853d90d01c575f8
Tyl-eer <blssdtyler03@gmail.com>
2020-12-16 21:29:27 +0300

    post requests

--- src/App.js
+++ src/App.js
@@ -1,39 +1,69 @@
 
 
  import React, {useState, useEffect} from 'react';
- //import Contacts from './components/contacts';
+//  import Contacts from './components/contacts';
  import Posts from './components/posts';
-
+import axios from 'axios';
 function App () {
   
-/*
-    const [contacts, setcontacts] = useState([])
-
-      useEffect(() => {
-        fetch('http://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then((data) => setcontacts(data))
-      
-        .catch(console.log (Error))
-      }, [])
-        console.log(contacts)
-      
-      
-        return (
-          <Contacts contacts={contacts} />
-        )
-      /** */
- 
-    const [posts, setposts] = useState([])
-
-      useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then((data) => setposts(data))
+    //  const [contacts, setcontacts] = useState([])
+       
+    //   useEffect(() => {
+    //     const users = 
+    //       {
+    //       "id": 11,
+    //       "name": "SAmuel ndirangu",
+    //       "username": "sam",
+    //       "email": "Sam@april.biz",
+    //       "address": {
+    //         "street": "Kulas Light",
+    //         "suite": "Apt. 556",
+    //         "city": "Gwenborough",
+    //         "zipcode": "92998-3874",
+    //         "geo": {
+    //           "lat": "-37.3159",
+    //           "lng": "81.1496"
+    //         }
+    //       },
+    //       "phone": "1-770-736-8031 x56442",
+    //       "website": "hildegard.org",
+    //       "company": {
+    //         "name": "Romaguera-Crona",
+    //         "catchPhrase": "Multi-layered client-server neural-net",
+    //         "bs": "harness real-time e-markets"
+    //       }
+    //     }
         
-        .catch(console.log (Error))
-      }, [])
+      //   axios.post('http://jsonplaceholder.typicode.com/users',users)
+      //   .then((response) => {
+         
+      //     setcontacts(response.data)
+      //     console.log(response.data)
+      //   }, (error) => {
+      //     console.log(error);
+      //   });
+      // }, [])
+      //   return (
+      //     <Contacts contacts={contacts} />
+      //   )
+ 
 
+
+
+
+      
+    const [posts, setposts] = useState([])
+     
+
+      useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+          setposts(response.data)
+          console.log(response.data)
+      }, (error) => {
+      console.log (error)
+      });
+      }, [])
         return (
           <Posts posts={posts} />
         )
--- src/components/contacts.js
+++ src/components/contacts.js
@@ -2,21 +2,24 @@
     import React from 'react'
 
     const Contacts = ({ contacts }) => {
+      console.log(contacts)
+
       return (
         <div>
           <center><h1>Contact List</h1></center>
-   
-          {contacts.map((contact) => (
+        
+          { 
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">{contact.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
-                <p class="card-text">{contact.company.catchPhrase}</p>
+                <h5 class="card-title">{contacts.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{contacts.email}</h6>
+                <p class="card-text">{contacts.phone}</p>
+                <h9 class="card-text">{contacts.website}</h9>
               </div>
             </div>
-          ))}
+          }
         </div>
       )
     };
 
-    export default Contacts
+    export default Contacts;
